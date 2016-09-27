8d7108e5183e3fd4dbb14a970f81628d3b04edad
tejraj <helloteju96@gmail.com>
2016-09-28 00:19:02 +0545

    Modified authorizer OauthServiceProvider.php mofified for user authorizer

--- app/Providers/OAuthServiceProvider.php
+++ app/Providers/OAuthServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Dingo\Api\Auth\Auth;
+use Dingo\Api\Auth\Provider\OAuth2;
+use App\User;
 
 class OAuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,21 @@ class OAuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app[Auth::class]->extend('oauth', function ($app) {
+            $provider = new OAuth2($app['oauth2-server.authorizer']->getChecker());
+
+            $provider->setUserResolver(function ($id) {
+                // Logic to return a user by their ID.
+                $user = User::find($id);
+                return $user;
+            });
+
+            $provider->setClientResolver(function ($id) {
+                // Logic to return a client by their ID.
+            });
+
+            return $provider;
+        });
     }
 
     /**
